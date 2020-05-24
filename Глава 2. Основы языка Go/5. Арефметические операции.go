d142f609f6b5c57242e309aed62b8fa4c273f308
shutov-mv <64813098+shutov-mv@users.noreply.github.com>
2020-05-24 09:14:21 +0300

    Create 5. Арефметические операции.go

---
+++ Глава 2. Основы языка Go/5. Арефметические операции.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		a float64 = 13
+		b float64 = 7
+		//c         = 55
+	)
+	//var f = a - b*c
+	//fmt.Println(f)
+	var g float64 = a / b
+	fmt.Println(g)
+	gg := 33
+	fmt.Println(gg)
+
+}
