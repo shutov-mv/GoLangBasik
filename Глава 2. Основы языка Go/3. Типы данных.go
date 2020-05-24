c3cce7bcdf143ee47b1a04e570beb476645c2c99
shutov-mv <64813098+shutov-mv@users.noreply.github.com>
2020-05-24 09:14:13 +0300

    Create 3. Типы данных.go

---
+++ Глава 2. Основы языка Go/3. Типы данных.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	var a int8 = -1
+	var b uint8 = 2
+	var c byte = 3
+	var d int16 = -4
+	var i uint16 = 5
+	var f int32 = -6
+	var g rune = -7
+	var aa float32 = -8
+	var bb float32 = -4.2
+	var cc float64 = 0.736
+	var dd complex64 = 2 - 1i
+	var ii complex128 = 4 + 3i
+}
