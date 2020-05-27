2ffbd8ba908e58703c870e518f4e91ec8a5d8f15
shutov-mv <64813098+shutov-mv@users.noreply.github.com>
2020-05-27 10:53:35 +0300

    Update 3. Вложенные структуры.go

--- Глава 4. Производные типы/3. Вложенные структуры.go
+++ Глава 4. Производные типы/3. Вложенные структуры.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type contact struct {
 	email string
 	phone string
@@ -10,14 +12,17 @@ type person struct {
 	age         int
 	contactInfo contact
 }
-func main () {
-	var tom = person {
+
+func main() {
+	var tom = person{
 		name: "Tom",
-		age: 24,
-		contactInfo: contact {
+		age:  24,
+		contactInfo: contact{
 			email: "tom@damain.local",
 			phone: "88887775454",
 		},
 	}
-	tom.contactInfo.email = 
+	tom.contactInfo.email = "stom@damain.local"
+	fmt.Println(tom.contactInfo.email)
+	fmt.Println(tom.contactInfo.phone)
 }
